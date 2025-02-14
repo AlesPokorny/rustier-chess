a69dfda68a7191d42b09c694fdbe25c0915a0889
Ales <ales.pokorny@coolblue.nl>
2025-02-14 19:01:35 +0100

    implement get_fen for Board

--- src/board.rs
+++ src/board.rs
@@ -457,6 +457,77 @@ impl Board {
         Ok(board)
     }
 
+    pub fn get_fen(&self) -> String {
+        let mut fen = String::new();
+
+        for i in (0..8).rev() {
+            let mut empty_squares = 0;
+            for j in 0..8 {
+                let square = Square::new(i * 8 + j);
+                match self.get_piece_on_square(&square) {
+                    Some(piece) => {
+                        if empty_squares > 0 {
+                            fen.push_str(&empty_squares.to_string());
+                        }
+                        fen.push_str(&piece.to_string());
+                        empty_squares = 0;
+                    }
+                    None => empty_squares += 1,
+                };
+            }
+
+            if empty_squares > 0 {
+                fen.push_str(&empty_squares.to_string())
+            }
+            if i > 0 {
+                fen.push('/');
+            }
+        }
+
+        fen.push(' ');
+
+        if self.state.turn == Color::WHITE {
+            fen.push('w');
+        } else {
+            fen.push('b');
+        }
+
+        fen.push(' ');
+
+        if self.state.castling.white_short() {
+            fen.push('K');
+        }
+        if self.state.castling.white_long() {
+            fen.push('Q');
+        }
+        if self.state.castling.black_short() {
+            fen.push('k');
+        }
+        if self.state.castling.black_long() {
+            fen.push('q');
+        }
+        if !self.state.castling.can_someone_castle() {
+            fen.push('-');
+        }
+
+        fen.push(' ');
+
+        match self.state.en_passant {
+            Some(square) => fen.push_str(&square.to_string()),
+            None => fen.push('-'),
+        }
+
+        fen.push(' ');
+
+        fen.push_str(&self.state.half_moves.to_string());
+
+        fen.push(' ');
+
+        fen.push_str(&self.state.full_moves.to_string());
+
+        fen
+    }
+
     fn get_piece_on_square(&self, square: &Square) -> Option<Piece> {
         let mut piece_color: usize = 3;
         for (i, color) in self.colors.iter().enumerate() {
@@ -505,5 +576,32 @@ impl fmt::Display for Board {
         write!(f, "     A   B   C   D   E   F   G   H")?;
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test_board {
+    use super::*;
+
+    #[test]
+    fn test_to_fen() {
+        let hasher = ZobristHasher::load();
+        let fens = [
+            "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+            "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
+            "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+            "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1",
+            "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+            "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+        ];
+
+        assert_eq!(
+            Board::new(&hasher).get_fen(),
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+        );
+
+        for fen in fens {
+            assert_eq!(Board::from_fen(fen, &hasher).unwrap().get_fen(), fen);
+        }
     }
 }
