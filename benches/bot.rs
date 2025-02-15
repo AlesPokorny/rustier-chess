345c88d51c1e81a3667e8b6fefb8dcc287873ab7
Ales <ales.pokorny@coolblue.nl>
2025-02-15 13:01:53 +0100

    Add bot benchmark

---
+++ benches/bot.rs
@@ -0,0 +1,18 @@
+use std::time::Instant;
+
+use rustier_chess::{
+    board::Board, bots::bot::Bot, moves::move_mask_gen::MoveGenMasks, utils::zobrist::ZobristHasher
+};
+
+fn main() {
+    let hasher = ZobristHasher::load();
+    let move_gen_masks = MoveGenMasks::load();
+    let board = Board::new(&hasher);
+    let mut bot = Bot::with_depth(7);
+
+    let now = Instant::now();
+    let _ = bot.get_best_move(&board, &move_gen_masks, &hasher);
+    let secs = now.elapsed().as_secs_f64();
+
+    println!("It took {} seconds", secs);
+}
